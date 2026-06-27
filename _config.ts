89a36a585a934d5f180276248b5073c9ed1d604e
HHYurdagul <hhyurdagul@gmail.com>
2026-06-27 09:18:38 +0300

    More components

--- _config.ts
+++ _config.ts
@@ -37,6 +37,7 @@ site.use(googleFonts({
   cssFile: "/css/fonts.css",
   fonts: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500&display=swap"
 }))
+site.use(lightningCss())
 
 site.use(shiki({
   highlighter: {
